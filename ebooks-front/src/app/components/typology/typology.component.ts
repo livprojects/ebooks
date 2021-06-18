2b99ebb29ac9f91709edb81c7e5f4a11dac2cc3b
Jaune9 <jonathan.triqueneau@gmail.com>
2021-06-18 08:53:01 +0200

    Ajout des components et modeles manquants d'hier

---
+++ ebooks-front/src/app/components/typology/typology.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-typology',
+  templateUrl: './typology.component.html',
+  styleUrls: ['./typology.component.css']
+})
+export class TypologyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ ebooks-front/src/app/models/typology.spec.ts
@@ -0,0 +1,7 @@
+import { Typology } from './typology';
+
+describe('Typology', () => {
+  it('should create an instance', () => {
+    expect(new Typology()).toBeTruthy();
+  });
+});
